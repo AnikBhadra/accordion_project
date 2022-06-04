1018815005f360e254210f98aaac487919f119fa
anik <anikbhadra01@gmail.com>
2022-06-04 17:32:49 +0530

    creating ui for first question

---
+++ components/Ui.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Ui() {
+    return (
+        <>
+            <main>
+                <div className="container">
+                    <h3>FAQ</h3>
+                    <section className="info">
+                        {/* will add questions here later */}
+                    </section>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Ui
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Ui from '../components/Ui'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-
+      <Ui />
     </div>
   )
 }
